ac0f8b1b1e877a1f94d51e32fa3116b0dcb18a3a
yutonakashima <y-nakashima@seassist.co.jp>
2024-10-01 22:35:55 +0900

    first commit

---
+++ src/main/java/com/example/demo/fizzBuzz/service/FizzBuzzService.java
@@ -0,0 +1,28 @@
+package com.example.demo.fizzBuzz.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FizzBuzzService {
+
+	public List<String> generateFizzBuzz(int limit) {
+		List<String> fizzBuzzList = new ArrayList<>();
+
+		for (int i = 1; i <= limit; i++) {
+			if (i % 15 == 0) {
+				fizzBuzzList.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				fizzBuzzList.add("Fizz");
+			} else if (i % 5 == 0) {
+				fizzBuzzList.add("Buzz");
+			} else {
+				fizzBuzzList.add(String.valueOf(i));
+			}
+		}
+
+		return fizzBuzzList;
+	}
+}
